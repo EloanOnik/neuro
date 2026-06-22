2c2b47ce0a880d83d016fc42988cab74fca9f8b9
Eloyans <eloanonik@gmail.com>
2026-06-22 15:50:24 +0400

    create Message

--- core/messages.py
+++ core/messages.py
@@ -1,13 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from uuid import uuid4
 from typing import Optional
 
 
 class Message(BaseModel):
-    message_id: str
-    default_factory = lambda: str(uuid4())
+    message_id: str = Field(default_factory=lambda: str(uuid4()))
     correlation_id: Optional[str] = None
-
 
 
 class UserUtterance(Message):
