e0337a4402bbeb9ba4563963417d1ef5c26cf9c8
Eloyans <eloanonik@gmail.com>
2026-06-22 15:32:06 +0400

    create EchoActor in actors/echo.py

--- actors/echo.py
+++ actors/echo.py
@@ -1,5 +1,8 @@
 from core.actor import Actor
 
 class EchoActor(Actor):
+    def __init__(self, name, system):
+        super().__init__(name)
+        self.system = system
     async def receive(self, message):
         print(f"{self.name} получил: {message}")
