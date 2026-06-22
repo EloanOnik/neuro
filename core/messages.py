f9c20df747e3121c8ef056f7d529b2e50fb7937e
Оник <onik232006@gmail.com>
2026-06-22 11:38:31 +0400

    add class Message

--- core/messages.py
+++ core/messages.py
@@ -1,4 +1,15 @@
 from pydantic import BaseModel
+from uuid import uuid4
+from typing import Optional
 
-class UserUtterance(BaseModel):
+
+class Message(BaseModel):
+    message_id: str
+    default_factory = lambda: str(uuid4())
+    correlation_id: Optional[str] = None
+
+
+
+class UserUtterance(Message):
     text: str
+    timestamp: float
