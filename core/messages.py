2355020839f0f152cd65fbc77e0073a31adafd3a
Eloyans <eloanonik@gmail.com>
2026-06-22 23:25:53 +0400

    add ShutdownCommand

--- core/messages.py
+++ core/messages.py
@@ -6,9 +6,16 @@ from typing import Optional
 class Message(BaseModel):
     message_id: str = Field(default_factory=lambda: str(uuid4()))
     correlation_id: Optional[str] = None
-    repty_to: Optional[str] = None
+    repty_to: Optional[str] = None # если актор не ждет ответа, но получатель хочет ответить
 
 
 class UserUtterance(Message):
     text: str
     timestamp: float
+
+# ShutdownCommand — системное сообщение: 
+# «актор, выйди из цикла, закончи работу». 
+# Оно обрабатывается прямо в _run() базового класса 
+# и не доходит до receive().
+class  ShutdownCommand(Message):
+    pass
