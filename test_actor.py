db94defad7d21c3693d4fc769d4deb16636dda6a
Eloyans <eloanonik@gmail.com>
2026-06-22 15:50:57 +0400

    create test_actor.py

--- test_actor.py
+++ test_actor.py
@@ -1,12 +1,17 @@
 import asyncio
 from actors.echo import EchoActor
+from core.system import ActorSystem
+from core.messages import UserUtterance
 
 async def main():
-    echo = EchoActor(name="echo")
-    await echo.start()
-    await echo.inbox.put("Привет")
-    await echo.inbox.put("еще одно сообение")
-    await asyncio.sleep(0.1)
-    await echo.stop()
+    system = ActorSystem()
+    echo = EchoActor(name="echo", system=system)
+    system.register(echo)
+    await system.start_all()
+    await system.tell("echo", UserUtterance(text="Привет", timestamp=0.0))
+    await asyncio.sleep(0.2)
+    await system.stop_all()
+
+
 
 asyncio.run(main())
