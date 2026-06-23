79a52efc441809e47cc1ab76e4cd57ca22189a5e
Eloyans <eloanonik@gmail.com>
2026-06-23 21:03:32 +0400

    add supervisor beta

--- core/supervisor.py
+++ core/supervisor.py
@@ -1,6 +1,25 @@
-
+import asyncio
 
 class Supervisor():
-    def __init__(self, restart_limit, restart_window):
-        restart_limit: int = 3
-        restart_window: float = 60.0
+    def __init__(self, restart_limit: int = 3, restart_window: float = 60.0):
+        self.restart_limit = restart_limit
+        self.restart_window = restart_window
+
+    async def watch(self, system, tasks, factories):
+        while True:
+            done, pending = await asyncio.wait(
+                tasks.values(),
+                return_when=asyncio.FIRST_EXCEPTION
+                )
+            
+            inverted_dict = {v: k for k, v in tasks.items()}
+            for task in done:
+                task_name = inverted_dict[task]
+
+                new_actor = factories[task_name]()
+                system.register(new_actor)
+                new_task = await new_actor.start()
+                tasks[task_name] = new_task
+                print(f"restart actor {task_name} after a crash")
+            
+        
